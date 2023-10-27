36f6c06a01a35b8b1169e5384273945510b38da0
Jonir Rings <i@jonirrings.com>
2023-10-27 18:00:05 +0800

    add i18n strings

--- u/MainWindow.xaml.cpp
+++ u/MainWindow.xaml.cpp
@@ -8,30 +8,21 @@
 #endif
 
 using namespace winrt;
-using namespace Microsoft::UI::Xaml;
+namespace winrt {
+	using namespace Microsoft::UI::Xaml;
+	using namespace Microsoft::Windows::ApplicationModel::Resources;
+}
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace winrt::u::implementation
 {
-    MainWindow::MainWindow()
-    {
-        InitializeComponent();
-    }
-
-    int32_t MainWindow::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
-    }
+	MainWindow::MainWindow()
+	{
+		InitializeComponent();
+		auto resourceLoader = ResourceLoader();
+		winrt::hstring t = resourceLoader.GetString(L"AppDisplayName");
+		Title(t);
+	}
 }
--- u/MainWindow.xaml.h
+++ u/MainWindow.xaml.h
@@ -11,10 +11,6 @@ namespace winrt::u::implementation
     {
         MainWindow();
 
-        int32_t MyProperty();
-        void MyProperty(int32_t value);
-
-        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
     };
 }
 
